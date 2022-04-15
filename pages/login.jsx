3a165428395f7f8c46403f7e1c5b9a6e3988f11e
michaelballos <mdb3091207@gmail.com>
2022-04-15 11:06:14 -0500

    RM
    Unused imports

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,40 +1,53 @@
-import { Stack, Center, Group, Button, Card, Input } from '@mantine/core'
+import { 
+  Stack,
+  Group,
+  Button,
+  Input
+} from '@mantine/core'
 
 const Login = () => {
   return (
-    <>
-      <Center>
-        <div>
-          <Stack>
-            <Input
-              style={{
-                width: '25vw',
-                padding: '.25rem',
-              }}
-              placeholder='Email'
-            />
-            <Input
-              style={{
-                width: '25vw',
-                padding: '.25rem',
-              }}
-              placeholder='Password'
-            />
-            <Group
-              style={{
-                display: 'flex',
-                justifyContent: 'space-around',
-                paddingLeft: '4rem',
-                paddingRight: '4rem',
-              }}
-            >
-              <Button variant='subtle'>Register</Button>
-              <Button variant='subtle'>Login</Button>
-            </Group>
-          </Stack>
-        </div>
-      </Center>
-    </>
+    <div
+      style={{
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+      }}
+    >
+    <Stack 
+      align='center' 
+      justify='center'
+    >
+      <Input
+        style={{
+          width: '25vw',
+          padding: '.25rem',
+        }}
+        placeholder='Email'
+      />
+      <Input
+        style={{
+          width: '25vw',
+          padding: '.25rem',
+        }}
+        placeholder='Password'
+      />
+      <Group position='center' spacing={'sm'}>
+        <Button
+          variant='subtle'
+        >
+          Register
+        </Button>
+        <Button
+          variant='subtle'
+        >
+          Login
+        </Button>
+      </Group>
+    </Stack>
+    </div>
   )
 }
 

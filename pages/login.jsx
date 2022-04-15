c150e68b470fc7f4c471dabef1758d0ab81f7426
michaelballos <mdb3091207@gmail.com>
2022-04-15 10:58:43 -0500

    Format

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,44 +1,39 @@
-import { Group, Button, Card, Input } from '@mantine/core'
+import { Stack, Center, Group, Button, Card, Input } from '@mantine/core'
 
 const Login = () => {
   return (
     <>
-      <Input
-        style={{
-          width: '25vw',
-          padding: '.25rem',
-        }}
-        placeholder='Email'
-      />
-      <Input
-        style={{
-          width: '25vw',
-          padding: '.25rem',
-        }}
-        placeholder='Password'
-      />
-      <Group
-        style={{
-          padding: '.25rem',
-        }}
-      >
-        <Button
-          style={{
-            width: '7vw',
-          }}
-          variant='subtle'
-        >
-          Register
-        </Button>
-        <Button
-          style={{
-            width: '7vw',
-          }}
-          variant='subtle'
-        >
-          Login
-        </Button>
-      </Group>
+      <Center>
+        <div>
+          <Stack>
+            <Input
+              style={{
+                width: '25vw',
+                padding: '.25rem',
+              }}
+              placeholder='Email'
+            />
+            <Input
+              style={{
+                width: '25vw',
+                padding: '.25rem',
+              }}
+              placeholder='Password'
+            />
+            <Group
+              style={{
+                display: 'flex',
+                justifyContent: 'space-around',
+                paddingLeft: '4rem',
+                paddingRight: '4rem',
+              }}
+            >
+              <Button variant='subtle'>Register</Button>
+              <Button variant='subtle'>Login</Button>
+            </Group>
+          </Stack>
+        </div>
+      </Center>
     </>
   )
 }

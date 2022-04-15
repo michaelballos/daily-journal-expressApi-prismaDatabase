4a2c786e62e7156b915c2b8c869beeb67fe8c940
michaelballos <mdb3091207@gmail.com>
2022-04-15 10:52:08 -0500

    RM
    Card and format

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,32 +1,45 @@
-import { Stack, Group, Card, Input } from '@mantine/core'
+import { Group, Button, Card, Input } from '@mantine/core'
+
 const Login = () => {
   return (
-  <Card
-    style={
-      {
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100%',
-      }
-    }
-  >
-    <Input
-      style={{
-        width: '25vw',
-        padding: '.25rem',
-      }}
-      placeholder='Email'
-    /> 
-    <Input
-      style={{
-        width: '25vw',
-        padding: '.25rem',
-      }}
-      placeholder='Password'
-    /> 
-  </Card>
+    <>
+      <Input
+        style={{
+          width: '25vw',
+          padding: '.25rem',
+        }}
+        placeholder='Email'
+      />
+      <Input
+        style={{
+          width: '25vw',
+          padding: '.25rem',
+        }}
+        placeholder='Password'
+      />
+      <Group
+        style={{
+          padding: '.25rem',
+        }}
+      >
+        <Button
+          style={{
+            width: '7vw',
+          }}
+          variant='subtle'
+        >
+          Register
+        </Button>
+        <Button
+          style={{
+            width: '7vw',
+          }}
+          variant='subtle'
+        >
+          Login
+        </Button>
+      </Group>
+    </>
   )
 }
 
